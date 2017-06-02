f6ff010cb5bcc8b0991d1beb93d75f6bdb44b2d1
Masato Takahashi <masato.takaha@gmail.com>
2017-06-02 10:32:58 -0700

    update the auth0 file

--- public/src/app/auth0-variables.ts
+++ public/src/app/auth0-variables.ts
@@ -10,6 +10,6 @@ export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: 'K67Sffss7Ejw12brFTu0bH4VpFz2Gdy8',
   CLIENT_DOMAIN: 'shrutha.auth0.com',
   AUDIENCE: 'ramen-taka-api',
-  REDIRECT: 'http://localhost:8000/callback',
+  REDIRECT: 'http://35.165.176.151//callback',
   SCOPE: 'openid profile'
 };
